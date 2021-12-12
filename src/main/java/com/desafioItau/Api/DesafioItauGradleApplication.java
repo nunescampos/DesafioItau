b37f388cf435e3300ddfd50ee9e6292a8a36467c
nunescampos <aluysio_88@hotmail.com>
2021-12-12 20:32:01 -0300

    first comit

---
+++ src/main/java/com/desafioItau/Api/DesafioItauGradleApplication.java
@@ -0,0 +1,13 @@
+package com.desafioItau.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItauGradleApplication {
+
+	public static void main(String[] args) {
+	cfW	SpringApplication.run(DesafioItauGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafioItau/Api/DesafioItauGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioItau.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
