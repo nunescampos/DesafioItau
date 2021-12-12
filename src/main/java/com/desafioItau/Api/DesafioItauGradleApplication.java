32305ea832f5b448b156083c7cd295cd01472434
nunescampos <aluysio_88@hotmail.com>
2021-12-12 20:47:18 -0300

    ccc

--- src/main/java/com/desafioItau/Api/DesafioItauGradleApplication.java
+++ src/main/java/com/desafioItau/Api/DesafioItauGradleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DesafioItauGradleApplication {
 
 	public static void main(String[] args) {
-	cfW	SpringApplication.run(DesafioItauGradleApplication.class, args);
+		SpringApplication.run(DesafioItauGradleApplication.class, args);
 	}
 
 }
