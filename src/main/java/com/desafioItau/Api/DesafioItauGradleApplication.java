7b57e5d7c945c00d5a33456527c13f969a65f6b9
nunescampos <aluysio_88@hotmail.com>
2021-12-12 21:03:19 -0300

    vvv

--- src/main/java/com/desafioItau/Api/DesafioItauGradleApplication.java
+++ src/main/java/com/desafioItau/Api/DesafioItauGradleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DesafioItauGradleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DesafioItauGradleApplication.class, args);
+		ddSpringApplication.run(DesafioItauGradleApplication.class, args);
 	}
 
 }
