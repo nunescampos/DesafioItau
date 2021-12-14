9eed5a9ee7780ca3c658826d4d120cffb1e4fe8a
nunescampos <aluysio_88@hotmail.com>
2021-12-14 14:14:49 -0300

    building

--- src/main/java/com/desafioItau/Api/repository/ClienteRepository.java
+++ src/main/java/com/desafioItau/Api/repository/ClienteRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import org.springframework.stereotype.Repository;
 
 import com.desafioItau.Api.entity.Cliente;
 
